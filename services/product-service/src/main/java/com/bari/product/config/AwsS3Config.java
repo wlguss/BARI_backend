5c866736426a591aa6f1145ea3d08183bee12146
wlguss <geum121554@konkuk.ac.kr>
2026-03-19 14:24:33 +0900

    feat: s3를 활용한 imageurl 등록기능 구현

--- services/product-service/src/main/java/com/bari/product/config/AwsS3Config.java
+++ services/product-service/src/main/java/com/bari/product/config/AwsS3Config.java
@@ -1,42 +1,21 @@
 package com.bari.product.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-
-import lombok.Getter;
-import lombok.Setter;
 
 @Configuration
 public class AwsS3Config {
 
-    @Bean
-    @ConfigurationProperties(prefix = "cloud.aws")
-    public AwsProperties awsProperties() {
-        return new AwsProperties();
-    }
+    @Value("${cloud.aws.region}")
+    private String region;
 
     @Bean
-    public S3Presigner s3Presigner(AwsProperties awsProperties) {
+    public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.of(awsProperties.getRegion()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(region))
                 .build();
-    }
-
-    @Getter
-    @Setter
-    public static class AwsProperties {
-        private String region;
-        private S3Properties s3 = new S3Properties();
-    }
-
-    @Getter
-    @Setter
-    public static class S3Properties {
-        private String bucket;
     }
 }
