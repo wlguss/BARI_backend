6279fb714ef36e7c67a02f6879ed32f60459921b
wlguss <http://github.com/wlguss>
2026-07-01 15:40:05 +0900

    chores: 불필요한 주석 제거

--- services/user-service/src/main/java/com/bari/user/config/SecurityConfig.java
+++ services/user-service/src/main/java/com/bari/user/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.bari.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
